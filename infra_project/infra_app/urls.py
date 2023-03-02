d47d8f1ca10d57110719772cfbbd0828457f3759
kirill31337 <104924471+kirill31337@users.noreply.github.com>
2023-03-02 19:30:09 +0300

    Update urls.py

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -2,11 +2,8 @@ from . import views
 
 from django.urls import path
 
-
 app_name = 'infra_app'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
-
 ]
