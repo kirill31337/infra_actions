dc86d1e5dcc2bc98ee635267ab476ed061298f12
kirill31337 <104924471+kirill31337@users.noreply.github.com>
2023-03-02 20:24:34 +0300

    Update views.py

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!11')
 
 
 def second_page(request):
